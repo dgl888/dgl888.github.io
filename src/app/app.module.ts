d598a80220e79ad8b33a53d1a88de14f3c317814
dgl888 <dglove@liberty.edu>
2023-06-29 18:02:55 -0400

    Added home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,8 @@
+import {HttpClientModule} from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,6 +10,7 @@ import { AppComponent } from './app.component';
 import { GradedComponent } from './components/graded/graded.component';
 import { QuizComponent } from './components/quiz/quiz.component';
 import { HomeComponent } from './components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,12 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
+import {HttpClient} from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {ICategory} from '../../shared/category.model';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public categories: ICategory[] = [];
+  public difficulties = [
+    'Easy',
+    'Medium',
+    'Hard'
+  ];
 
-  constructor() { }
+  public selectedCategory: string = 'none';
+  public selectedDifficulty: string = 'none';
+  private apiUrl = 'https://opentdb.com/api_category.php';
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.http.get<any>(this.apiUrl).subscribe((data) => {
+      this.categories = data.trivia_categories;
+    });
+  }
+
+  createQuiz() {
+    let id  = this.categories.find(category => category.name === this.selectedCategory)?.id;
+    this.router.navigate(['quiz'], {queryParams: {
+        category: id, categoryName: this.selectedCategory, difficulty: this.selectedDifficulty
+      }});
+  }
+
+  onCategorySelected(value: string) {
+    this.selectedCategory = value;
+  }
+
+
+  onDifficultySelected(value: string) {
+    this.selectedDifficulty = value;
   }
 
 }
